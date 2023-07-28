5ba272ea8b36e1e69cae8dcbc015b1ca6f5f4794
ravithejajs <neteller012@outlook.com>
2023-07-28 18:44:45 +0530

    new ec2

--- deploy-bundle/applicationstart.sh
+++ deploy-bundle/applicationstart.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -d -p --name vprofile_app 8080:8080 484472757370.dkr.ecr.ap-south-1.amazonaws.com/vprofile-qa:vprofileapp-%version%
+docker run -d -p --name vprofile_app -p 8080:8080 484472757370.dkr.ecr.ap-south-1.amazonaws.com/vprofile-qa:vprofileapp-%version%
